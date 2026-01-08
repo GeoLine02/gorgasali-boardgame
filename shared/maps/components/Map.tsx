2b4545f3b8f994e8d854d8ba91a423f6a94f457a
mjavanagoale-creator <mjavanagoale@gmail.com>
2026-01-09 03:29:09 +0400

    create-map-cell's

---
+++ shared/maps/components/Map.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import MapCell from "./MapCell";
+import { url } from "inspector";
+
+const Map = () => {
+  return (
+    <div className="flex max-w-[1350px] flex-wrap bg-[url(/map-one.png)] bg-no-repeat object-cover aspect-square">
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+      <MapCell />
+    </div>
+  );
+};
+
+export default Map;
---
+++ shared/maps/components/MapCell.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MapCell = () => {
+  return (
+    <div className="border-2 border-gray-400 w-10 h-10 cursor-pointer"></div>
+  );
+};
+
+export default MapCell;
